eeb053352c2f9a4886c7501ae9f9b309ce9a4d04
GabrielaPanzarini <gabriela.panzarini@outlook.com>
2023-01-29 11:26:10 -0300

    commit full project

---
+++ PacoteDownloads/public/js/api.js
@@ -0,0 +1,8 @@
+let api_key = "9a9f5f3fc46048b1c9dfaf92f61a306e"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
+
